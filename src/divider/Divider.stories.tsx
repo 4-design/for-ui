d09dc665eac17095a9823470214083040eae41c6
Miyamae Yuuya <locona.0810@gmail.com>
2021-06-23 18:53:28 +0900

    feat(Divider): Divider 実装 (#71)

---
+++ src/divider/Divider.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Meta } from '@storybook/react/types-6-0'
+
+import 'twin.macro'
+import { Divider } from './Divider'
+
+export default {
+  title: 'Atom/Divider',
+  component: Divider,
+  decorators: [
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (Story: any) => (
+      <div tw="mt-10 flex flex-col h-screen w-screen gap-4">
+        <Story />
+      </div>
+    ),
+  ],
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as Meta
+
+export const basic = () => (
+  <div>
+    <Divider />
+  </div>
+)
