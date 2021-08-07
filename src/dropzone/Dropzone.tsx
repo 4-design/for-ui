b90267ce90f87b1f713d544c407191be22c6bb11
wheeltwice <wheeltwice@gmail.com>
2021-08-07 19:04:33 +0900

    fix(Dropzone): dropzoneのファイル名省略 (#179)

--- src/dropzone/Dropzone.tsx
+++ src/dropzone/Dropzone.tsx
@@ -34,14 +34,15 @@ export const Dropzone: React.VFC<DropzoneProps> = ({
     >
       <input {...getInputProps()} />
       {files.length > 0 ? (
-        <div>
+        <div tw="w-full flex justify-center">
           {files.map((file) => (
-            <div key={file.name} tw="flex justify-center items-center">
-              <div tw="mr-2">{file.name}</div>
+            <div key={file.name} tw="flex justify-center items-center w-4/5">
+              <div tw="mr-2 truncate">{file.name}</div>
               <Button
                 onClick={(e) => onRemove(file)(e)}
                 color="danger"
                 variant="text"
+                twin={[tw`flex-shrink-0 flex-grow-0`]}
               >
                 <MdClose size={24} />
               </Button>
@@ -49,7 +50,7 @@ export const Dropzone: React.VFC<DropzoneProps> = ({
           ))}
         </div>
       ) : (
-        <div tw="flex flex-col items-center text-middle">
+        <div tw="w-full flex flex-col items-center text-middle">
           <MdFileUpload size={48} tw="mb-3" />
           <p tw="text-middle">{message}</p>
         </div>
