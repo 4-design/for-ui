2e7e42aae1a673bdd71dc9e1d4c0cf9ba1b19be3
Miyamae Yuuya <locona.0810@gmail.com>
2021-12-04 01:00:11 +0900

    feat: v2 Chip 実装 (#309)
    * feat: v2 Chip 実装
    
    * fix:
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix

---
+++ src/chip/Chip.stories.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Meta } from '@storybook/react/types-6-0'
+
+import 'twin.macro'
+import { Text } from '../typography/Typography'
+import { Chip } from './Chip'
+
+export default {
+  title: 'Atom/Chip',
+  component: Chip,
+  decorators: [
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (Story: any) => (
+      <div tw="mt-10 flex flex-col h-screen w-screen gap-4">
+        <Story />
+      </div>
+    ),
+  ],
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as Meta
+
+export const basic = () => (
+  <div tw="flex flex-col">
+    <div tw="border-b mb-4">
+      <Text variant="h3">Chip</Text>
+    </div>
+
+    <div tw="flex flex-col gap-4">
+      <div>
+        <Chip label="ラベル" />
+      </div>
+      <div>
+        <Chip label="ラベル" onDelete={() => console.log('delete')} />
+      </div>
+    </div>
+  </div>
+)
---
+++ src/chip/Chip.tsx
@@ -0,0 +1,38 @@
+import { VFC } from 'react'
+import MuiChip, { ChipProps as MuiChipProps } from '@mui/material/Chip'
+import tw, { css, TwStyle } from 'twin.macro'
+
+export type ChipProps = Exclude<MuiChipProps, 'color'> & {
+  twin?: TwStyle[]
+  color?: 'default' | 'negative'
+}
+
+const styles: TwStyle = {
+  default: tw`bg-shade-light-default`,
+  negative: tw`bg-negative-light-default`,
+}
+
+export const Chip: VFC<ChipProps> = ({ color = 'default', twin, ...props }) => {
+  return (
+    <MuiChip
+      css={[
+        css`
+          &.MuiChip-root {
+            ${tw`border border-shade-medium-default`}
+            ${styles[color]}
+            ${twin}
+
+            > .MuiChip-label {
+              ${tw`font-sans text-s text-shade-dark-default`}
+            }
+
+            > .MuiChip-deleteIcon {
+              ${tw`text-shade-dark-default`}
+            }
+          }
+        `,
+      ]}
+      {...props}
+    />
+  )
+}
