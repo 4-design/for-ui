6bb83c7e5915ea446971c82fd6bb13da41d1a1d6
Miyamae Yuuya <locona.0810@gmail.com>
2021-12-16 17:29:34 +0900

    fix(Modal): TailwindCSS デフォルトのfocus-visibleをオーバーライト

--- src/modal/Modal.tsx
+++ src/modal/Modal.tsx
@@ -1,7 +1,6 @@
 import React, { ReactNode } from 'react'
 import { SerializedStyles } from '@emotion/react'
 import ModalUnstyled from '@mui/base/ModalUnstyled'
-import Backdrop from '@mui/material/Backdrop'
 
 import tw, { styled, TwStyle } from 'twin.macro'
 
@@ -18,10 +17,6 @@ export type ModalProps = {
   onClose?(event: React.MouseEvent | React.KeyboardEvent): void
 }
 
-const StyledBackdrop = styled(Backdrop)`
-  background-color: rgba(0, 0, 0, 0.2);
-`
-
 const StyledModal = styled(ModalUnstyled)`
   position: fixed;
   z-index: 1300;
@@ -32,6 +27,17 @@ const StyledModal = styled(ModalUnstyled)`
   display: flex;
   align-items: center;
   justify-content: center;
+`
+
+const Backdrop = styled('div')`
+  z-index: -1;
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+  -webkit-tap-highlight-color: transparent;
 `
 
 export const Modal: React.VFC<ModalProps> = ({
@@ -46,10 +52,14 @@ export const Modal: React.VFC<ModalProps> = ({
       open={open}
       onClose={onClose}
       css={[rootTwin]}
-      BackdropComponent={StyledBackdrop}
+      BackdropComponent={Backdrop}
       BackdropProps={{ onClick: onClose }}
     >
-      <div css={[tw`flex justify-center min-h-screen`]}>
+      <div
+        css={[
+          tw`flex justify-center min-h-screen focus-visible:ring-0 focus-visible:outline-none`,
+        ]}
+      >
         <div
           css={[
             tw`flex flex-col m-auto rounded-lg shadow-modal break-all transition-all transform bg-shade-white-default`,
