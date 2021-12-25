a52d87c17ca3d45222be58681ede0fcbf085f50f
Miyamae Yuuya <locona.0810@gmail.com>
2021-12-26 03:49:35 +0900

    fix(Sidebar): support customize (#398)

--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -5,19 +5,22 @@ export type SidebarMenuItemProps = {
   icon: React.ReactNode
   label: string | React.ReactNode // NOTE: 一時的に文字列とコンポーネントを許容
   active?: boolean
+  twin?: TwStyle
 }
 
 export const SidebarMenuItem: React.FC<SidebarMenuItemProps> = ({
   icon,
   label,
   active = false,
+  twin,
 }) => (
   <div
     css={[
       tw`flex items-center mx-4 px-2 py-3 text-r font-medium transition duration-300 ease-in-out rounded-lg cursor-pointer text-shade-medium-default hover:text-primary-white-default`,
+
       css`
         & > svg {
-          ${tw`mr-4`}
+          ${tw`mr-2`}
         }
 
         &:hover {
@@ -28,6 +31,7 @@ export const SidebarMenuItem: React.FC<SidebarMenuItemProps> = ({
         css`
           ${tw`text-primary-white-default bg-primary-dark-hover`}
         `,
+      twin,
     ]}
   >
     {icon}
@@ -47,15 +51,15 @@ export const SidebarMenu: React.FC<SidebarMenuProps> = ({ children }) => {
 }
 
 export type SidebarProps = {
-  twin?: TwStyle
   logo: React.ReactNode
+  twin?: TwStyle
   children: React.ReactNode
 }
 
-export const Sidebar: React.FC<SidebarProps> = ({ logo, children }) => {
+export const Sidebar: React.FC<SidebarProps> = ({ logo, twin, children }) => {
   return (
     <div tw="h-screen flex flex-shrink-0">
-      <div tw="flex flex-col w-64 bg-primary-dark-default">
+      <div css={[tw`flex flex-col w-56 bg-primary-dark-default`, twin]}>
         <div tw="flex flex-col flex-grow overflow-y-auto">
           {logo}
           {children}
