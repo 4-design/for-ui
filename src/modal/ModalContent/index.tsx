4c6adf7ded5275cad6394930eda296f331ea3156
Miyamae Yuuya <locona.0810@gmail.com>
2022-02-11 14:21:53 +0900

    fix(Modal/ModalContent):  remove using p tag

--- src/modal/ModalContent/index.tsx
+++ src/modal/ModalContent/index.tsx
@@ -1,18 +1,6 @@
-import React, { ReactNode } from 'react'
+import React from 'react'
 import 'twin.macro'
-import { Text } from '../../typography/Typography'
 
-interface Props {
-  /** The contents of the Dialog. */
-  children: ReactNode | string
-}
-
-export const ModalContent: React.FC<Props> = ({ children }) => {
-  return (
-    <div tw="p-4">
-      <Text variant="p" bold={false}>
-        {children}
-      </Text>
-    </div>
-  )
+export const ModalContent: React.FC = ({ children }) => {
+  return <div tw="p-4">{children}</div>
 }
