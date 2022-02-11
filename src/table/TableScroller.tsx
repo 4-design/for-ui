e3d06c68f8fb9a72f700088482c759d2767d3b48
Miyamae Yuuya <locona.0810@gmail.com>
2022-02-11 15:46:10 +0900

    feat(Table): disablePagination / TableScroller 実装 (#545)

---
+++ src/table/TableScroller.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import tw, { css } from 'twin.macro'
+
+type Props = {
+  height?: number | string
+}
+
+export const TableScroller: React.FC<Props> = ({ height, children }) => (
+  <div
+    css={[
+      tw`overflow-y-auto`,
+      typeof height === 'string' &&
+        css`
+          height: ${height};
+        `,
+      typeof height === 'number' &&
+        css`
+          height: ${height}px;
+        `,
+      css`
+        & table {
+          > thead {
+            & th {
+              position: sticky;
+              top: 0;
+              z-index: 999;
+            }
+          }
+        }
+      `,
+    ]}
+  >
+    {children}
+  </div>
+)
