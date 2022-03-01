f807b75f53d0d2be790533e9dfa0b836639d4b4c
Yuumi Natsubori <44659317+O-natsu@users.noreply.github.com>
2022-03-01 13:48:26 +0900

    feat(Stepper): add stepper component (#556)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/stepper/Step.tsx
@@ -0,0 +1,77 @@
+import React, { forwardRef } from 'react'
+import MuiStep, { StepProps as MuiStepProps } from '@mui/material/Step'
+import { StepIconProps as MuiStepIconProps } from '@mui/material/StepIcon'
+import MuiStepLabel, {
+  StepLabelProps as MuiStepLabelProps,
+} from '@mui/material/StepLabel'
+import tw, { css, TwStyle } from 'twin.macro'
+
+export interface StepProps extends MuiStepProps {
+  twin?: TwStyle[]
+}
+
+export interface StepLabelProps extends MuiStepLabelProps {
+  twin?: TwStyle[]
+}
+
+export const Step = forwardRef<HTMLDivElement, StepProps & StepLabelProps>(
+  (props, ref) => {
+    const { twin, children, ...rest } = props
+    return (
+      <MuiStep ref={ref} css={twin} {...rest}>
+        <MuiStepLabel
+          ref={ref}
+          StepIconComponent={Icon}
+          css={[
+            css`
+              & .MuiStepLabel-label {
+                ${tw`text-shade-dark-default text-r font-normal`}
+                font-family: inherit;
+                &.Mui-completed,
+                &.Mui-active {
+                  ${tw`text-shade-dark-default text-r font-normal`}
+                }
+              }
+              & .MuiStepLabel-alternativeLabel {
+                ${tw`mt-2!`}
+              }
+            `,
+            twin,
+          ]}
+        >
+          {children}
+        </MuiStepLabel>
+      </MuiStep>
+    )
+  }
+)
+
+const Icon = (props: Partial<MuiStepIconProps>) => {
+  const { completed, active, icon } = props
+
+  return (
+    <>
+      <span
+        css={[
+          css`
+            ${tw`relative h-8 w-8 border-2 border-primary-dark-disabled text-shade-dark-disabled bg-shade-white-disabled font-bold text-r rounded-full`}
+            ${active &&
+            tw`border-2 border-primary-dark-default text-primary-dark-default bg-shade-white-default`}
+          ${completed &&
+            tw`border-0 text-shade-white-default bg-primary-dark-default`}
+          `,
+        ]}
+      >
+        <span
+          css={[
+            css`
+              ${tw`absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4`}
+            `,
+          ]}
+        >
+          {icon}
+        </span>
+      </span>
+    </>
+  )
+}
---
+++ src/stepper/Stepper.tsx
@@ -0,0 +1,44 @@
+import { forwardRef } from 'react'
+import MuiStepper, {
+  StepperProps as MuiStepperProps,
+} from '@mui/material/Stepper'
+import tw, { css, TwStyle } from 'twin.macro'
+
+export interface StepperProps extends MuiStepperProps {
+  twin?: TwStyle[]
+}
+
+export const Stepper = forwardRef<HTMLDivElement, StepperProps>(
+  ({ twin, activeStep, alternativeLabel, children, ...rest }, ref) => {
+    return (
+      <MuiStepper
+        ref={ref}
+        activeStep={activeStep}
+        alternativeLabel={alternativeLabel}
+        css={[
+          css`
+            & .MuiStepConnector-root {
+              ${tw`top-6 px-0 left-[calc(-50% + 1rem)] right-[calc(50% + 1rem)]`}
+              & .MuiStepConnector-line {
+                ${tw`border-t-2 border-shade-dark-disabled`}
+              }
+              &.Mui-completed .MuiStepConnector-line {
+                ${tw`border-shade-dark-default`}
+              }
+              &.Mui-active .MuiStepConnector-line {
+                ${tw`border-shade-dark-default`}
+              }
+              &.Mui-disabled .MuiStepConnector-line {
+                ${tw`border-shade-dark-disabled`}
+              }
+            }
+          `,
+          twin,
+        ]}
+        {...rest}
+      >
+        {children}
+      </MuiStepper>
+    )
+  }
+)
