ef09f547afce9ccc853c17b6d0a0f06b2c1d9804
Miyamae Yuuya <locona.0810@gmail.com>
2022-05-16 13:53:32 +0900

    feat(Tabs): Tabs 実装 / Tabs -> TabList に変更 (#604)

--- src/tab/TabList.tsx
+++ src/tab/TabList.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { SerializedStyles } from '@emotion/react'
-import MuiTabs, { TabsProps as MuiTabsProps } from '@mui/material/Tabs'
+import MuiTabList, { TabListProps as MuiTabListProps } from '@mui/lab/TabList'
 import tw, { css, TwStyle } from 'twin.macro'
 
-export interface TabsProps extends MuiTabsProps {
+export interface TabListProps extends MuiTabListProps {
   noBorder?: boolean
   reverse?: boolean
   color?: 'primary' | 'secondary' | 'shade'
@@ -16,21 +16,19 @@ const colorStyle = {
   shade: tw`bg-shade-medium-default`,
 }
 
-export const Tabs: React.VFC<TabsProps> = ({
+export const TabList: React.VFC<TabListProps> = ({
   twin,
   noBorder = false,
   reverse = false,
   color = 'secondary',
-  value,
   onChange,
   children,
   ...rest
 }) => {
-  console.info('value', value)
   return (
-    <MuiTabs
-      value={value}
+    <MuiTabList
       onChange={onChange}
+      aria-label="lab API tabs example"
       css={[
         css`
           &.MuiTabs-root {
@@ -49,6 +47,6 @@ export const Tabs: React.VFC<TabsProps> = ({
       {...rest}
     >
       {children}
-    </MuiTabs>
+    </MuiTabList>
   )
 }
