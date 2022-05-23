fade805ea3dd802729412396004a8a89784127ff
locona <locona.0810@gmail.com>
2022-05-23 17:47:18 +0900

    fix

---
+++ .storybook/manager.js
@@ -0,0 +1,10 @@
+import '@storybook/addon-backgrounds/register'
+import { addons } from '@storybook/addons'
+import { create } from '@storybook/theming'
+
+addons.setConfig({
+  theme: create({
+    base: 'light',
+    brandTitle: '3design-ui',
+  }),
+})
