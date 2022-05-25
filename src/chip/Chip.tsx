61ecd88c098c55772414caef4410ae8fadf825b4
Qs-F <qsf@de-liker.com>
2022-05-25 19:31:09 +0900

    Cheery-picked 9f0b8474559aec69c575c15eb8834f4f2a4b351f and Merge into refactor/update-chip-component

--- src/chip/Chip.tsx
+++ src/chip/Chip.tsx
@@ -4,18 +4,29 @@ import tw, { css, TwStyle, theme } from 'twin.macro'
 
 type ChipColorType = 'default' | 'negative' | 'white'
 
-export type ChipProps = Exclude<MuiChipProps, 'color'> & {
+export type ChipProps = Exclude<
+  MuiChipProps,
+  'color' | 'onDelete' | 'icon' | 'deleteIcon' | 'size'
+> & {
   twin?: TwStyle[]
   color?: ChipColorType
-  leadingIcon?: MuiChipProps['icon']
-  trailingIcon?: MuiChipProps['deleteIcon']
-  isEndIcon?: boolean
-}
+} & (
+    | {
+        leadingIcon: MuiChipProps['icon']
+        trailingIcon?: never
+      }
+    | {
+        leadingIcon?: never
+        trailingIcon?: MuiChipProps['deleteIcon']
+        onDelete?: MuiChipProps['onDelete']
+      }
+  )
 
-const styles = {
+const styles = (clickable: boolean) => ({
   negative: css`
     &.MuiChip-root {
-      ${tw`bg-negative-light-default hover:bg-negative-light-hover`};
+      ${tw`bg-negative-light-default`};
+      ${clickable && tw`hover:bg-negative-light-hover cursor-pointer`};
       > .MuiChip-label {
         ${tw`text-negative-medium-default`};
       }
@@ -25,73 +36,80 @@ const styles = {
       > .MuiChip-deleteIcon {
         ${tw`text-negative-medium-default`};
         &:hover {
-          ${tw`opacity-70`}
+          ${clickable && tw`opacity-70`};
         }
       }
     }
   `,
   white: css`
-        &.MuiChip-root {
-          ${tw`border-solid border-shade-light-default bg-shade-white-default hover:bg-shade-light-hover`};
-          > .MuiChip-label {
-            ${tw`text-shade-dark-default`};
-          }
-          > .MuiChip-icon {
-            color: ${theme`iconColor.shade.dark.default`};
-          }
-          > .MuiChip-deleteIcon {
-            ${tw`text-shade-dark-default`}
-            &:hover {
-              color: ${theme`iconColor.primary.dark.hover`} !important;
-          }
-        }
-      `,
-  default: css`
     &.MuiChip-root {
-      ${tw`border-shade-medium-default bg-shade-light-default hover:bg-shade-light-hover`}
+      ${tw`border-solid border-shade-light-default bg-shade-white-default`};
+      ${clickable && tw`hover:bg-shade-light-hover cursor-pointer`};
       > .MuiChip-label {
-        ${tw`text-shade-dark-default`}
+        ${tw`text-shade-dark-default`};
       }
       > .MuiChip-icon {
         color: ${theme`iconColor.shade.dark.default`};
       }
       > .MuiChip-deleteIcon {
-        ${tw`text-shade-dark-default`}
-        &:hover {
-          color: ${theme`iconColor.primary.dark.hover`} !important;
-        }
+        ${tw`text-shade-dark-default`};
+        ${clickable &&
+        `&:hover {
+              color: ${theme`iconColor.primary.dark.hover`} !important;
+            }`}
       }
     }
   `,
-}
+  default: css`
+    &.MuiChip-root {
+      ${tw`border-shade-medium-default bg-shade-light-default`};
+      ${clickable && tw`hover:bg-shade-light-hover cursor-pointer`};
+      > .MuiChip-label {
+        ${tw`text-shade-dark-default`};
+      }
+      > .MuiChip-icon {
+        color: ${theme`iconColor.shade.dark.default`};
+      }
+      > .MuiChip-deleteIcon {
+        ${tw`text-shade-dark-default`};
+        ${clickable &&
+        `&:hover {
+              color: ${theme`iconColor.primary.dark.hover`} !important;
+            }`}
+      }
+    }
+  `,
+})
 
 export const Chip: FC<ChipProps> = ({
   color = 'default',
   leadingIcon,
-  isEndIcon,
+  trailingIcon,
+  clickable,
+  onDelete,
   twin,
   ...props
 }) => {
   return (
     <MuiChip
-      icon={leadingIcon}
+      icon={!onDelete ? leadingIcon || trailingIcon : undefined}
+      deleteIcon={trailingIcon}
+      onDelete={onDelete}
       css={[
         css`
           &.MuiChip-root {
-            ${tw`border px-3 py-1 h-7`}
-            ${styles[`${color}`]}
-            ${isEndIcon && tw`flex-row-reverse`}
-            ${twin}
-            > .MuiChip-label {
+            ${tw`border px-3 py-1 h-7 inline-flex gap-1`}
+            ${leadingIcon && tw`pl-2`}
+              ${(trailingIcon || onDelete) && tw`pr-2`}
+              ${styles(!!(clickable || onDelete))[`${color}`]}
+              ${!onDelete && trailingIcon ? tw`flex-row-reverse` : ''}
+              ${twin}
+              > .MuiChip-label {
               ${tw`font-sans text-s px-0`}
-              ${(leadingIcon || isEndIcon) && tw`pr-0!`}
             }
-            > .MuiChip-deleteIcon {
-              ${tw`ml-0 -mr-1.5`}
-            }
-            .MuiChip-icon {
-              ${leadingIcon && tw`ml-0 mr-1`}
-              ${isEndIcon && tw`ml-1 mr-0`}
+            > .MuiChip-deleteIcon,
+            > .MuiChip-icon {
+              ${tw`m-0`}
             }
           }
         `,
