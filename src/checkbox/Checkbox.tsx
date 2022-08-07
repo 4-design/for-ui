a12c7887f396f558cb38d535cb9c38c83bb7504d
locona <locona.0810@gmail.com>
2022-08-07 23:20:00 +0900

    feat: Checkbox実装

--- src/checkbox/Checkbox.tsx
+++ src/checkbox/Checkbox.tsx
@@ -1,8 +1,9 @@
-import React, { FC, memo } from 'react'
+import { FC } from 'react'
 import MuiCheckbox, {
   CheckboxProps as MuiCheckboxProps,
 } from '@mui/material/Checkbox'
 import FormControlLabel from '@mui/material/FormControlLabel'
+import clsx from 'clsx'
 import { Typography } from '../typography'
 
 export type CheckboxProps = MuiCheckboxProps & {
@@ -10,52 +11,41 @@ export type CheckboxProps = MuiCheckboxProps & {
   nopadding?: boolean
 }
 
-const _Checkbox: FC<CheckboxProps> = memo(({ nopadding = false, ...rest }) => (
+const _Checkbox: FC<CheckboxProps> = ({ nopadding = false, ...rest }) => (
   <MuiCheckbox
-    // sx={{ '& .MuiSvgIcon-root': { fontSize: 20 } }}
-    // css={[
-    //   css`
-    //     &.MuiCheckbox-root {
-    //       ${tw`(text-primary-dark-default)!`}
-    //       ${nopadding && tw`p-0!`}
-
-    //       color: ${theme`textColor.shade.medium.default`} !important;
-
-    //       &.Mui-checked {
-    //         color: ${theme`backgroundColor.secondary.dark.default`} !important;
-    //       }
-
-    //       &.Mui-disabled > span path {
-    //         ${tw`text-shade-dark-disabled!`}
-    //       }
-    //     }
-    //   `,
-    // ]}
+    sx={{ '& .MuiSvgIcon-root': { fontSize: 20 } }}
+    classes={{
+      root: clsx(['text-shade-medium-default', nopadding && 'p-0']),
+      checked: clsx(['text-secondary-dark-default']),
+      disabled: clsx(['text-shade-dark-disabled']),
+    }}
     {...rest}
   />
-))
-
-export const Checkbox = memo(
-  ({ label, nopadding = false, ...rest }: CheckboxProps) => {
-    return (
-      <>
-        {label ? (
-          <FormControlLabel
-            control={<_Checkbox nopadding={nopadding} {...rest} />}
-            label={
-              <Typography
-                variant="body1"
-                disabled={rest.disabled}
-                className={`text-s`}
-              >
-                {label}
-              </Typography>
-            }
-          />
-        ) : (
-          <_Checkbox nopadding={nopadding} {...rest} />
-        )}
-      </>
-    )
-  }
 )
+
+export const Checkbox = ({
+  label,
+  nopadding = false,
+  ...rest
+}: CheckboxProps) => {
+  return (
+    <>
+      {label ? (
+        <FormControlLabel
+          control={<_Checkbox nopadding={nopadding} {...rest} />}
+          label={
+            <Typography
+              variant="body1"
+              disabled={rest.disabled}
+              className={`text-s`}
+            >
+              {label}
+            </Typography>
+          }
+        />
+      ) : (
+        <_Checkbox nopadding={nopadding} {...rest} />
+      )}
+    </>
+  )
+}
