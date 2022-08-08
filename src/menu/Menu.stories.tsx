0816ce0e78fd38613a698384dd5c77357f8cabed
locona <locona.0810@gmail.com>
2022-08-08 12:06:27 +0900

    feat: Menu 実装

---
+++ src/menu/Menu.stories.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { Meta } from '@storybook/react/types-6-0'
+
+import { Button } from '../button/Button'
+import { Text } from '../typography/Typography'
+import { Menu } from './Menu'
+import { MenuItem } from './MenuItem'
+
+export default {
+  title: 'Navigation / Menu',
+  component: Menu,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+  decorators: [
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (Story: any) => (
+      <div className="mt-10 flex h-screen w-screen flex-col gap-4">
+        <Story />
+      </div>
+    ),
+  ],
+} as Meta
+
+export const Base = () => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
+  const toggle = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(anchorEl ? null : event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  const open = Boolean(anchorEl)
+
+  return (
+    <div className="flex flex-col gap-8">
+      <div className="mb-4 border-b">
+        <Text variant="h3">Button/Contained/Large</Text>
+      </div>
+
+      <div>
+        <Button
+          id="demo-positioned-button"
+          aria-controls="demo-positioned-menu"
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+          variant="contained"
+          onClick={toggle}
+        >
+          プロジェクト
+        </Button>
+
+        <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
+          <MenuItem>
+            <a href="#">プロフィール</a>
+          </MenuItem>
+          <MenuItem>
+            <a href="#">設定</a>
+          </MenuItem>
+          <MenuItem>
+            <a href="#">プロフィール</a>
+          </MenuItem>
+        </Menu>
+      </div>
+    </div>
+  )
+}
