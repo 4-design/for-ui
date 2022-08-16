024138010bb306fdf0ea34d081b6d027e2ba3b92
Miyamae Yuuya <locona.0810@gmail.com>
2022-08-16 14:39:44 +0900

    feat: Radio実装 (#716)
    * feat: Radio実装
    
    * fix: ref
    
    * fix: labelをhoverしてもスタイルがgroup-hoverが適応されるように変更

--- src/radio/Radio.tsx
+++ src/radio/Radio.tsx
@@ -1,6 +1,7 @@
 import { FC, Fragment, memo, forwardRef } from 'react'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import MuiRadio, { RadioProps as MuiRadioProps } from '@mui/material/Radio'
+import clsx from 'clsx'
 
 export interface RadioProps extends MuiRadioProps {
   label?: string
@@ -8,46 +9,35 @@ export interface RadioProps extends MuiRadioProps {
   nopadding?: boolean
 }
 
-const Indicator: FC<{ checked: boolean; disabled: boolean }> = () => (
+const Indicator: FC<{ checked: boolean; disabled: boolean }> = ({
+  checked,
+  disabled,
+}) => (
   <span
-  // css={[
-  //   tw`w-5 h-5 bg-shade-white-default rounded-full transition-[border-width] duration-100`,
-  //   checked ? tw`border-7` : tw`border-2`,
-  //   disabled
-  //     ? tw`border-shade-medium-disabled`
-  //     : checked
-  //     ? tw`border-secondary-dark-default`
-  //     : tw`border-shade-medium-default`,
-  // ]}
+    className={clsx([
+      'h-5 w-5 rounded-full bg-shade-white-default transition-[border-width] duration-100',
+      checked
+        ? 'border-7'
+        : 'border-2 group-hover:border-3 group-hover:border-secondary-dark-default',
+      disabled
+        ? 'border-shade-medium-disabled'
+        : checked
+        ? 'border-secondary-dark-default'
+        : 'border-shade-medium-default',
+    ])}
   />
 )
 
-const _Radio: FC<RadioProps> = memo(({ disabled, ...rest }) => (
+const _Radio: FC<RadioProps> = memo(({ nopadding, disabled, ref, ...rest }) => (
   <MuiRadio
     disableRipple
     icon={<Indicator checked={false} disabled={!!disabled} />}
     checkedIcon={<Indicator checked={true} disabled={!!disabled} />}
     disabled={disabled}
-    // css={[
-    //   css`
-    //     &.MuiRadio-root {
-    //       ${nopadding ? tw`p-0!` : tw`p-1`}
-    //       ${
-    //         /* Hover style for Indicator. Did not work well if written in Indicator component. */ ''
-    //       }
-    //       &:hover:not(.Mui-checked) span {
-    //         ${tw`border-secondary-dark-default border-3`};
-    //       }
-    //       &:hover {
-    //         ${tw`bg-transparent`};
-    //       }
-    //       &.Mui-focusVisible span {
-    //         ${tw`outline-focus`};
-    //       }
-    //     }
-    //   `,
-    //   twin,
-    // ]}
+    classes={{
+      root: clsx(['group hover:bg-transparent', nopadding ? 'p-0' : 'p-1']),
+    }}
+    inputRef={ref}
     {...rest}
   />
 ))
@@ -61,19 +51,15 @@ export const Radio: FC<RadioProps> = forwardRef(
             disabled={disabled}
             value={value}
             label={label}
-            control={<_Radio {...rest} />}
+            control={
+              <_Radio value={value} disabled={disabled} ref={ref} {...rest} />
+            }
             ref={ref}
-            // css={[
-            //   css`
-            //     ${tw`flex gap-2 m-0`};
-            //     & .MuiFormControlLabel-label {
-            //       ${tw`(font-sans text-s text-shade-dark-default)!`};
-            //       &.Mui-disabled {
-            //         ${tw`text-shade-dark-disabled!`};
-            //       }
-            //     }
-            //   `,
-            // ]}
+            classes={{
+              root: clsx(['group m-0 flex gap-2']),
+              label: clsx(['font-sans text-s text-shade-dark-default']),
+              disabled: clsx(['text-shade-dark-disabled']),
+            }}
           />
         ) : (
           <_Radio value={value} disabled={disabled} ref={ref} {...rest} />
