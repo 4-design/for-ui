38ced8859a0a75b3fd00a03f7d118b15c8c63899
Miyamae Yuuya <locona.0810@gmail.com>
2022-08-17 00:42:55 +0900

    feat: Header / Sidebar 廃止 ・ Example 追加 (#721)
    * feat: Layout 実装
    
    * fix
    
    * feat: example

---
+++ example/src/Sidebar.tsx
@@ -0,0 +1,87 @@
+/* eslint-disable tailwindcss/no-custom-classname */
+import React from 'react'
+import clsx from 'clsx'
+import {
+  SidebarContext,
+  SidebarProvider,
+  useSidebarContext,
+} from './SidebarContext'
+
+export type SidebarMenuItemProps = {
+  IconComponent: React.ElementType
+  label: string | React.ReactNode // NOTE: 一時的に文字列とコンポーネントを許容
+  active?: boolean
+  className?: string
+}
+
+export const SidebarMenuItem: React.FC<SidebarMenuItemProps> = ({
+  IconComponent,
+  label,
+  className,
+  active = false,
+}) => {
+  const { open } = useSidebarContext()
+  return (
+    <div
+      // eslint-disable-next-line tailwindcss/classnames-order
+      className={clsx([
+        'flex cursor-pointer items-center px-4 py-3 text-r font-medium',
+        'hover:text-primary-white-default',
+        active
+          ? 'rounded-none border-0 border-l-4 border-solid border-l-secondary-medium-active bg-primary-dark-active text-primary-white-default hover:bg-primary-dark-hover'
+          : 'text-shade-light-default',
+        className,
+      ])}
+    >
+      {/* {IconElement} */}
+      <IconComponent size={24} className={clsx([open ? 'mr-4' : 'mr-0'])} />
+
+      {open ? label : null}
+    </div>
+  )
+}
+
+export type SidebarMenuProps = {
+  children: React.ReactNode
+}
+
+export const SidebarMenu: React.FC<SidebarMenuProps> = ({ children }) => {
+  return (
+    <nav className="flex h-0 flex-1 flex-col overflow-y-auto pb-2">
+      {children}
+    </nav>
+  )
+}
+
+export type SidebarProps = {
+  LogoComponent: React.ElementType<{ open: boolean }>
+  children: React.ReactElement
+  defaultOpen?: boolean
+}
+
+export const Sidebar: React.FC<SidebarProps> = ({
+  LogoComponent,
+  defaultOpen = true,
+  children,
+}) => {
+  return (
+    <SidebarProvider defaultOpen={defaultOpen}>
+      <SidebarContext.Consumer>
+        {({ open }) => (
+          <div
+            className={clsx([
+              'mx-0 flex h-screen w-14 shrink-0 grow bg-shade-dark-default',
+              'transition-slowest transition-width duration-300 ease-in-out',
+              open ? 'w-60' : 'w-[56px]',
+            ])}
+          >
+            <div className="flex grow flex-col overflow-y-auto bg-primary-dark-default">
+              <LogoComponent open={open} />
+              {children}
+            </div>
+          </div>
+        )}
+      </SidebarContext.Consumer>
+    </SidebarProvider>
+  )
+}
---
+++ example/src/SidebarContext.tsx
@@ -0,0 +1,63 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import React from 'react'
+
+type UseSidebar = {
+  open: boolean
+  handleOpen: (event: React.MouseEvent<HTMLElement>) => void
+  handleClose: (event: React.MouseEvent<HTMLElement>) => void
+}
+
+const useSidebar = (defaultOpen = true): UseSidebar => {
+  const [open, setOpen] = React.useState(defaultOpen)
+
+  const handleOpen = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault()
+    e.stopPropagation()
+
+    console.info('open')
+
+    setOpen(true)
+  }
+
+  const handleClose = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault()
+    e.stopPropagation()
+
+    setOpen(false)
+  }
+
+  return {
+    open,
+    handleOpen,
+    handleClose,
+  }
+}
+
+type SidebarContext = UseSidebar
+
+export const SidebarContext = React.createContext<SidebarContext>({
+  open: true,
+  handleOpen: () => {},
+  handleClose: () => {},
+})
+
+export const useSidebarContext = (): SidebarContext =>
+  React.useContext(SidebarContext)
+
+type SidebarProviderProps = {
+  defaultOpen?: boolean
+  children: React.ReactElement
+}
+
+export const SidebarProvider: React.FC<SidebarProviderProps> = ({
+  defaultOpen = true,
+  children,
+}) => {
+  const { open, handleOpen, handleClose } = useSidebar(defaultOpen)
+
+  return (
+    <SidebarContext.Provider value={{ open, handleOpen, handleClose }}>
+      {React.cloneElement(children, { open, handleOpen, handleClose })}
+    </SidebarContext.Provider>
+  )
+}
