11498741d3582f38a461f18bcfff339be1d31d6d
Miyamae Yuuya <locona.0810@gmail.com>
2022-08-18 16:16:08 +0900

    fix: icon property バグ修正 / font-size remを使用 > tailwindcss.config.js (#729)

--- tailwind.config.js
+++ tailwind.config.js
@@ -326,7 +326,7 @@ const fontSizes = {
     },
   ],
   xr: [
-    '1.25em',
+    '1.25rem',
     {
       lineHeight: '1.75rem',
       letterSpacing: '.03rem',
@@ -357,14 +357,14 @@ module.exports = {
         icon: (value) => {
           if (!corePlugins('iconOpacity')) {
             return {
-              'icon-color': toColorValue(value),
+              'color': toColorValue(value),
             }
           }
 
           return withAlphaVariable({
             color: value,
-            property: 'icon-color',
-            variable: '--tw-bg-opacity',
+            property: 'color',
+            variable: '--tw-color-opacity',
           })
         },
       },
