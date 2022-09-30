bb219a704a22efc95ed1ee44b2ea584ccc90f1f8
Miyamae Yuuya <locona.0810@gmail.com>
2022-09-30 14:32:20 +0900

    fix(eslint-config): remove custom import/order and unused-imports (#798)

--- packages/eslint-config/src/index.cjs
+++ packages/eslint-config/src/index.cjs
@@ -10,7 +10,7 @@ module.exports = {
       files: '*.{,c,m}{j,t}s{,x}',
       parser: '@typescript-eslint/parser',
       extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-      plugins: ['sonarjs', 'unicorn', 'promise', 'import', 'unused-imports'],
+      plugins: ['sonarjs', 'unicorn', 'promise', 'import'],
       rules: {
         // Disallows if statements as the only statement in else blocks
         // https://eslint.org/docs/rules/no-lonely-if
@@ -49,40 +49,7 @@ module.exports = {
 
         'import/no-default-export': 'error',
         'import/prefer-default-export': 'off', // disable opposite of 'import/no-default-export'
-        'import/order': [
-          'error',
-          {
-            groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-            pathGroupsExcludedImportTypes: ['react'],
-            pathGroups: [
-              {
-                pattern: 'react',
-                group: 'builtin',
-                position: 'before',
-              },
-              {
-                pattern: '/**',
-                group: 'internal',
-                position: 'after',
-              },
-            ],
-            alphabetize: {
-              order: 'asc',
-              caseInsensitive: true,
-            },
-          },
-        ],
-
         '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
-        'unused-imports/no-unused-vars': [
-          'warn',
-          {
-            vars: 'all',
-            varsIgnorePattern: '^_',
-            args: 'after-used',
-            argsIgnorePattern: '^_',
-          },
-        ],
       },
       settings: {
         'import/extensions': ['.ts', '.tsx'],
