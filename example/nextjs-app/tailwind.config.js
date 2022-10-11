4c418f86f0ecfe7abb58640762a8a6b29ac14df7
locona <locona.0810@gmail.com>
2022-10-11 20:37:16 +0900

    fix: lint

--- example/nextjs-app/tailwind.config.js
+++ example/nextjs-app/tailwind.config.js
@@ -1,10 +1,7 @@
 module.exports = {
   important: '#__next',
   presets: [require('@4design/for-ui/tailwind.config.base.js')],
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
