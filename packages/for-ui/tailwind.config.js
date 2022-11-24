57dabb8066f217f95985441200efbd659cb5e085
Qs-F <qsf@de-liker.com>
2022-11-24 19:23:48 +0900

    Fix to get tailwind work in story in docs mode

--- packages/for-ui/tailwind.config.js
+++ packages/for-ui/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  important: '#root',
+  important: ['#root', '#docs-root'],
   presets: [require('./tailwind.config.base.js')],
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
 };
