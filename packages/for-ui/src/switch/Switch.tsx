5d250cc9390f1e9f84b9b3ef4cdd2a391f91a9f0
MaQueen <67810971+kotobuki562@users.noreply.github.com>
2022-12-09 06:57:13 +0900

    switctのサイズを変更

--- packages/for-ui/src/switch/Switch.tsx
+++ packages/for-ui/src/switch/Switch.tsx
@@ -16,8 +16,9 @@ export const Switch: React.FC<SwitchProps> = ({ value, checked, disabled, ...res
           value={value}
           checked={checked}
           disabled={disabled}
+          // toggleの可動域を狭めるために、trackのサイズを変更する
           classes={{
-            root: fsx(['my-2 mr-2 h-6 w-11 p-0']),
+            root: fsx(['my-2 mr-2 h-5 w-8 p-0.5']),
             track: fsx([
               'bg-primary-dark-default block h-full w-full rounded-xl opacity-100',
               checked && 'bg-secondary-dark-default opacity-100',
@@ -25,8 +26,10 @@ export const Switch: React.FC<SwitchProps> = ({ value, checked, disabled, ...res
               checked && disabled && 'bg-secondary-dark-disabled opacity-100',
             ]),
             thumb: fsx([
-              'bg-shade-white-default absolute top-1 left-1 h-4 w-4 rounded-2xl transition-all duration-200 ease-in',
+              'bg-shade-white-default absolute top-1 left-1 h-3 w-3 rounded-2xl transition-all duration-200 ease-in-out',
+              checked && 'transform -translate-x-2 ease-in-out',
             ]),
+            input: fsx(['w-8 h-5']),
           }}
         />
       }
