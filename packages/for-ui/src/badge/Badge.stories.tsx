7be56f810f6541ebdbfac3eef2265065e7273bbc
MaQueen <67810971+kotobuki562@users.noreply.github.com>
2022-12-26 21:14:55 +0900

    バッジコンポーネントの実装

--- packages/for-ui/src/badge/Badge.stories.tsx
+++ packages/for-ui/src/badge/Badge.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
-import { MdMail } from 'react-icons/md';
+import { MdPersonOutline } from 'react-icons/md';
 import { Badge } from './Badge';
 
 export default {
@@ -9,9 +9,33 @@ export default {
 } as Meta;
 
 const Template: Story = () => (
-  <Badge badgeContent={4}>
-    <MdMail size={24} />
-  </Badge>
+  <div className="flex flex-row gap-8">
+    <div className="flex flex-col gap-4">
+      <Badge label="注意書き" />
+      <Badge label="注意書き" color="primary" />
+      <Badge label="注意書き" color="native" />
+    </div>
+    <div className="flex flex-col gap-4">
+      <Badge label="神田 柚奈" color="white" type="text" icon={<MdPersonOutline className="h-4 w-4" />} />
+      <Badge label="神田 柚奈" color="white" type="text" />
+      <Badge label="神田 柚奈" color="gray" type="text" icon={<MdPersonOutline className="h-4 w-4" />} />
+      <Badge label="神田 柚奈" color="gray" type="text" />
+      <Badge label="神田 柚奈" color="primary" type="text" icon={<MdPersonOutline className="h-4 w-4" />} />
+      <Badge label="神田 柚奈" color="primary" type="text" />
+      <Badge label="神田 柚奈" color="native" type="text" icon={<MdPersonOutline className="h-4 w-4" />} />
+      <Badge label="神田 柚奈" color="native" type="text" />
+    </div>
+    <div className="flex flex-col gap-4">
+      <Badge label="神田 柚奈" color="white" type="outline" icon={<MdPersonOutline className="h-4 w-4" />} />
+      <Badge label="神田 柚奈" color="white" type="outline" />
+      <Badge label="神田 柚奈" color="gray" type="outline" icon={<MdPersonOutline className="h-4 w-4" />} />
+      <Badge label="神田 柚奈" color="gray" type="outline" />
+      <Badge label="神田 柚奈" color="primary" type="outline" icon={<MdPersonOutline className="h-4 w-4" />} />
+      <Badge label="神田 柚奈" color="primary" type="outline" />
+      <Badge label="神田 柚奈" color="native" type="outline" icon={<MdPersonOutline className="h-4 w-4" />} />
+      <Badge label="神田 柚奈" color="native" type="outline" />
+    </div>
+  </div>
 );
 
 export const DefaultBadge = Template.bind({});
--- packages/for-ui/src/badge/Badge.tsx
+++ packages/for-ui/src/badge/Badge.tsx
@@ -1,25 +1,88 @@
 import React, { ReactNode } from 'react';
-import { BadgeProps } from '@mui/material/Badge';
-import MuiBadge from '@mui/material/Badge';
 
-interface Props extends BadgeProps {
-  className?: string;
-  children: ReactNode;
-}
+import { MdError } from 'react-icons/md';
+import { fsx } from '../system/fsx';
 
-export const Badge: React.FC<Props> = ({ className, badgeContent, children, ...rest }) => {
-  return (
-    <MuiBadge
-      badgeContent={badgeContent}
-      color="primary"
-      componentsProps={{
-        root: {
-          className: `text-shade-dark-default ${className}`,
-        },
-      }}
-      {...rest}
-    >
-      {children}
-    </MuiBadge>
-  );
+type Props = {
+  color?: 'white' | 'gray' | 'primary' | 'native';
+  icon?: ReactNode;
+  type?: 'constant' | 'text' | 'outline';
+  label: string;
+};
+
+export const Badge: React.FC<Props> = ({ color = 'white', icon, type = 'constant', label }) => {
+  switch (type) {
+    case 'constant':
+      return (
+        <div
+          className={fsx([
+            'flex flex-row items-start gap-1 rounded',
+            color === 'white' && 'text-[#0B0D0E]',
+            color === 'primary' && 'text-[#005093]',
+            color === 'native' && 'text-[#C00F50]',
+          ])}
+        >
+          <div className="flex h-6 w-4 flex-row items-start py-1">
+            <MdError className="h-4 w-4" />
+          </div>
+          <span className="text-r font-bold">{label}</span>
+        </div>
+      );
+    case 'text':
+      return (
+        <div
+          className={fsx([
+            'flex flex-row items-center w-fit gap-1 text-r justify-center rounded px-2',
+            color === 'white' && 'bg-[#fff] text-[#0B0D0E]',
+            color === 'gray' && 'bg-[#F7F8F8] text-[#0B0D0E]',
+            color === 'primary' && 'bg-[#E0F4FD] text-[#005093]',
+            color === 'native' && 'bg-[#FBEEF1] text-[#C00F50]',
+          ])}
+        >
+          {icon && (
+            <div
+              className={fsx([
+                color === 'white' && 'text-[#B3BCC1]',
+                color === 'gray' && 'text-[#B3BCC1]',
+                color === 'primary' && 'text-[#7ACFF4]',
+                color === 'native' && 'text-[#F58DA9]',
+              ])}
+            >
+              {icon}
+            </div>
+          )}
+
+          <span>{label}</span>
+        </div>
+      );
+    case 'outline':
+      return (
+        <div
+          className={fsx([
+            'flex flex-row items-center w-fit gap-1 text-r justify-center rounded outline px-2',
+            color === 'white' && 'bg-[#fff] text-[#0B0D0E] outline-[#EEF0F1]',
+            color === 'gray' && 'bg-[#F7F8F8] text-[#0B0D0E] outline-[#EEF0F1]',
+            color === 'primary' && 'bg-[#E0F4FD] text-[#005093] outline-[#AFE2F8]',
+            color === 'native' && 'bg-[#FBEEF1] text-[#C00F50] outline-[#F9BACB]',
+          ])}
+        >
+          {icon && (
+            <div
+              className={fsx([
+                color === 'white' && 'text-[#B3BCC1]',
+                color === 'gray' && 'text-[#B3BCC1]',
+                color === 'primary' && 'text-[#7ACFF4]',
+                color === 'native' && 'text-[#F58DA9]',
+              ])}
+            >
+              {icon}
+            </div>
+          )}
+
+          <span>{label}</span>
+        </div>
+      );
+    default:
+      return null;
+  }
 };
