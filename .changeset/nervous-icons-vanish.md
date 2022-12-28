994e05d9258f28a8926fbe653c7b60c62fcd48f0
たふみ <qsf@de-liker.com>
2022-12-28 14:27:58 +0900

    fix(TextArea): `helperText` と `label` propsを追加 (#957)
    * Add label and helperText props for TextArea
    
    * Add change log

---
+++ .changeset/nervous-icons-vanish.md
@@ -0,0 +1,5 @@
+---
+"@4design/for-ui": patch
+---
+
+fix(TextArea): `helperText` と `label` propsを追加
--- packages/for-ui/src/textArea/TextArea.stories.tsx
+++ packages/for-ui/src/textArea/TextArea.stories.tsx
@@ -39,58 +39,81 @@ export const Default = (): JSX.Element => {
       <Text as="h3" size="l" weight="bold">
         TextArea
       </Text>
-      <Text as="label" size="s" weight="bold" className="text-shade-medium-default flex flex-col gap-1">
-        3行固定TextArea
-        <TextArea
-          rows={3}
-          autoComplete="on"
-          placeholder="4design@example.com"
-          error={!!errors['email']}
-          {...register('email')}
-        />
-      </Text>
-      <Text as="label" size="s" weight="bold" className="text-shade-medium-default flex flex-col gap-1">
-        改行すると行が増えるTextArea
-        <TextArea
-          required
-          autoComplete="on"
-          placeholder="4design@example.com"
-          error={!!errors['email']}
-          {...register('email')}
-        />
-      </Text>
-      <Text as="label" size="s" weight="bold" className="text-shade-medium-default flex flex-col gap-1">
-        最小2行 最大4行 TextArea
-        <TextArea
-          required
-          minRows={2}
-          maxRows={4}
-          autoComplete="on"
-          placeholder="4design@example.com"
-          error={!!errors['email']}
-          {...register('email')}
-        />
-      </Text>
-      <Text as="label" size="s" weight="bold" className="text-shade-medium-default flex flex-col gap-1">
-        <Text>
-          エラー
-          <Text className="text-negative-medium-default" weight="regular">
-            *
+
+      <TextArea
+        rows={3}
+        label="3行固定TextArea"
+        autoComplete="on"
+        placeholder="4design@example.com"
+        error={!!errors['email']}
+        {...register('email')}
+      />
+
+      <TextArea
+        label={
+          <Text>
+            改行すると行が増えるTextArea
+            <Text
+              weight="regular"
+              size="xs"
+              className="border-negative-medium-default text-negative-medium-default ml-1 rounded-full border px-1"
+            >
+              必須
+            </Text>
           </Text>
-        </Text>
-        <TextArea error autoComplete="on" placeholder="4design@example.com" {...register('email')} />
-      </Text>
-      <Text as="label" size="s" weight="bold" className="text-shade-medium-default flex flex-col gap-1">
+        }
+        required
+        autoComplete="on"
+        placeholder="4design@example.com"
+        error={!!errors['email']}
+        {...register('email')}
+      />
+
+      <TextArea
+        label="最小2行 最大4行 TextArea"
+        required
+        minRows={2}
+        maxRows={4}
+        autoComplete="on"
+        placeholder="4design@example.com"
+        error={!!errors['email']}
+        helperText="注: たくさん入力しても4行以上にはなりません"
+        {...register('email')}
+      />
+
+      <TextArea
+        label={
+          <Text size="l" weight="regular" className="text-shade-light-default">
+            エラー
+            <Text className="text-negative-medium-default" weight="regular">
+              *
+            </Text>
+          </Text>
+        }
+        error
+        autoComplete="on"
+        placeholder="4design@example.com"
+        {...register('email')}
+      />
+
+      <TextArea
+        error
+        autoComplete="on"
+        placeholder="4design@example.com"
+        {...register('email')}
+        label="エラー"
+        helperText="エラーがでてます"
+      />
+
+      <TextArea
+        label="disabled"
+        rows={3}
+        autoComplete="on"
+        placeholder="4design@example.com"
+        error={!!errors['email']}
         disabled
-        <TextArea
-          rows={3}
-          autoComplete="on"
-          placeholder="4design@example.com"
-          error={!!errors['email']}
-          disabled
-          {...register('email')}
-        />
-      </Text>
+        {...register('email')}
+      />
       <Button type="submit">登録する</Button>
     </form>
   );
