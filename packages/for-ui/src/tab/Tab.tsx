4c307056c8f39a88c445566f67e5c9ea47442090
Qs-F <qsf@de-liker.com>
2023-01-12 18:23:25 +0900

    Fix to use styles inherited by Tabs or TabList

--- packages/for-ui/src/tab/Tab.tsx
+++ packages/for-ui/src/tab/Tab.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
+import { FC } from 'react';
 import MuiTab, { TabProps as MuiTabProps } from '@mui/material/Tab';
 import { fsx } from '../system/fsx';
 
-export interface TabProps extends MuiTabProps {
+export interface TabProps extends Omit<MuiTabProps, 'disableRipple'> {
   minWidth?: number;
 }
 
-export const Tab: React.VFC<TabProps> = ({ minWidth = 160, disabled, tabIndex, ...rest }) => {
-  return (
-    <MuiTab
-      classes={{
-        root: fsx([
-          `min-w-[${minWidth}] text-r text-primary-medium-default min-h-[auto] pt-2 pb-2.5 font-sans font-bold`,
-        ]),
-        textColorPrimary: fsx(['focus:outline-none']),
-        selected: fsx(['text-primary-dark-default focus:outline-none']),
-        wrapped: fsx(['focus:outline-none']),
-      }}
-      disabled={disabled}
-      tabIndex={tabIndex}
-      {...rest}
-    />
-  );
-};
+export const Tab: FC<TabProps> = ({ minWidth, ...rest }) => (
+  <MuiTab
+    disableRipple
+    className={fsx(
+      `text-shade-medium-default font-bold text-r py-1.5 px-4 min-h-min border-transparent border-solid border-y-2 normal-case transition-all duration-100`
+    )}
+    sx={{ minWidth }}
+    {...rest}
+  />
+);
