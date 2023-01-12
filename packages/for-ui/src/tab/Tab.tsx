ed1e73c2d0d2ecb3a7eef80a46ec64019b10c94a
Qs-F <qsf@de-liker.com>
2023-01-12 19:15:39 +0900

    Fix default tabIndex to 0 to support physical keyboard selection

--- packages/for-ui/src/tab/Tab.tsx
+++ packages/for-ui/src/tab/Tab.tsx
@@ -6,12 +6,14 @@ export interface TabProps extends Omit<MuiTabProps, 'disableRipple'> {
   minWidth?: number;
 }
 
-export const Tab: FC<TabProps> = ({ minWidth, ...rest }) => (
+export const Tab: FC<TabProps> = ({ minWidth, tabIndex, className, ...rest }) => (
   <MuiTab
     disableRipple
     className={fsx(
-      `text-shade-medium-default font-bold text-r py-1.5 px-4 min-h-min border-transparent border-solid border-y-2 normal-case transition-all duration-100`
+      `text-shade-medium-default font-bold text-r py-1.5 px-4 min-h-min border-transparent border-solid border-y-2 normal-case transition-all duration-100 [&:focus-visible]:bg-shade-white-active`,
+      className
     )}
+    tabIndex={tabIndex ?? 0}
     sx={{ minWidth }}
     {...rest}
   />
