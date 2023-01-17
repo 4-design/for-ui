a25540e50d3b10560ba4e3a36173c67d1b0d1fd9
locona <locona.0810@gmail.com>
2023-01-17 19:42:34 +0900

    resolve conflict

--- example/nextjs-app/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  important: '#__next',
-  presets: [require('@4design/for-ui/tailwind.config.base.js')],
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
