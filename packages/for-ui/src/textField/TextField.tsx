93281b5fadcb661bc66cc06e55bfbe0231abea7c
Qs-F <qsf@de-liker.com>
2023-01-21 18:49:14 +0900

    Fix TextField to react when clicking label

--- packages/for-ui/src/textField/TextField.tsx
+++ packages/for-ui/src/textField/TextField.tsx
@@ -89,7 +89,7 @@ export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
         endAdornment: (
           <InputAdornment
             classes={{
-              root: fsx(['text-r text-shade-dark-default mr-3 font-sans antialiased']),
+              root: fsx(['text-r text-shade-dark-default mr-3 font-sans']),
             }}
             position="start"
             disableTypography
@@ -123,72 +123,70 @@ export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
     );
 
     return (
-      <div className={fsx('flex flex-col w-full', className)}>
-        <label>
-          {label && (
-            <Text as="label" className={fsx(['text-s text-shade-medium-default mb-1 font-bold antialiased'])}>
-              {label}
-              {required && <Text className="text-negative-medium-default">*</Text>}
-            </Text>
-          )}
-          <MuiTextField
-            disabled={disabled}
-            error={error}
-            inputRef={validRef}
-            required={required}
-            variant="outlined"
-            placeholder={placeholder}
-            sx={{
-              '& .MuiInputBase-input:disabled::placeholder': {
-                '-webkit-text-fill-color': 'currentColor',
-              },
-            }}
-            className={fsx(`w-full flex flex-col gap-1`)}
-            FormHelperTextProps={{
-              classes: {
-                root: fsx([error && 'text-negative-medium-default m-0 text-s']),
-              },
-            }}
-            InputProps={{
-              classes: {
-                root: fsx(['group bg-shade-white-default text-shade-light-default px-0 antialiased']),
-                disabled: fsx(['bg-shade-white-disabled', 'placeholder:text-shade-light-default']),
-                input: fsx([
-                  'text-r text-shade-dark-default placeholder:text-shade-light-default h-auto py-2.5 px-3 font-sans placeholder:opacity-100 focus:shadow-none',
-                ]),
-                focused: fsx(['border-primary-medium-active']),
-                notchedOutline: fsx([
-                  'border',
-                  disabled
-                    ? 'border-shade-medium-disabled'
-                    : error
-                    ? 'border-negative-medium-default'
-                    : _focused
-                    ? 'border-2 border-primary-medium-active group-hover:border-primary-dark-default'
-                    : 'border-shade-medium-default group-hover:border-primary-dark-default',
-                ]),
-                inputAdornedEnd: fsx(['pr-2']),
-              },
-              startAdornment: prefix && (
-                <InputAdornment
-                  position="start"
-                  classes={{
-                    root: 'border-r border-shade-light-default bg-shade-light-default max-h-[fit-content] h-full py-2.5 px-3 m-0',
-                  }}
-                >
-                  <Text className="text-shade-dark-default inline-flex">{prefix}</Text>
-                </InputAdornment>
-              ),
-              ...InputProps,
-              ..._InputProps,
-            }}
-            inputProps={inputProps}
-            onFocus={handleFocus}
-            onBlur={handleBlur}
-            {...rest}
-          />
-        </label>
-      </div>
+      <Text as="label" className={fsx('flex flex-col w-full gap-1', className)}>
+        {label && (
+          <Text className={fsx(['text-s text-shade-medium-default font-bold'])}>
+            {label}
+            {required && <Text className="text-negative-medium-default">*</Text>}
+          </Text>
+        )}
+        <MuiTextField
+          disabled={disabled}
+          error={error}
+          inputRef={validRef}
+          required={required}
+          variant="outlined"
+          placeholder={placeholder}
+          sx={{
+            '& .MuiInputBase-input:disabled::placeholder': {
+              '-webkit-text-fill-color': 'currentColor',
+            },
+          }}
+          className={fsx(`w-full flex flex-col gap-1`)}
+          FormHelperTextProps={{
+            classes: {
+              root: fsx([error && 'text-negative-medium-default m-0 text-s']),
+            },
+          }}
+          InputProps={{
+            classes: {
+              root: fsx(['group bg-shade-white-default text-shade-light-default px-0 antialiased']),
+              disabled: fsx(['bg-shade-white-disabled', 'placeholder:text-shade-light-default']),
+              input: fsx([
+                'text-r text-shade-dark-default placeholder:text-shade-light-default h-auto py-2.5 px-3 font-sans placeholder:opacity-100 focus:shadow-none',
+              ]),
+              focused: fsx(['border-primary-medium-active']),
+              notchedOutline: fsx([
+                'border',
+                disabled
+                  ? 'border-shade-medium-disabled'
+                  : error
+                  ? 'border-negative-medium-default'
+                  : _focused
+                  ? 'border-2 border-primary-medium-active group-hover:border-primary-dark-default'
+                  : 'border-shade-medium-default group-hover:border-primary-dark-default',
+              ]),
+              inputAdornedEnd: fsx(['pr-2']),
+            },
+            startAdornment: prefix && (
+              <InputAdornment
+                position="start"
+                classes={{
+                  root: 'border-r border-shade-light-default bg-shade-light-default max-h-[fit-content] h-full py-2.5 px-3 m-0',
+                }}
+              >
+                <Text className="text-shade-dark-default inline-flex">{prefix}</Text>
+              </InputAdornment>
+            ),
+            ...InputProps,
+            ..._InputProps,
+          }}
+          inputProps={inputProps}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          {...rest}
+        />
+      </Text>
     );
   }
 );
