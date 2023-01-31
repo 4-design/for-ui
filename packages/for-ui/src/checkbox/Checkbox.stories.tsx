d04fa2c7240eb9e302795b910b5108a12ae39ba7
Qs-F <qsf@de-liker.com>
2023-01-31 19:35:23 +0900

    fix(Checkbox): Add CheckboxGroup and CustomLabel story

--- packages/for-ui/src/checkbox/Checkbox.stories.tsx
+++ packages/for-ui/src/checkbox/Checkbox.stories.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Meta } from '@storybook/react/types-6-0';
 import { useForm } from 'react-hook-form';
-
-import { Button } from '../button';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { fsx } from '../system/fsx';
 import { Checkbox } from './Checkbox';
-import { LegacyText as Text } from '../typography';
+import { CheckboxGroup } from './CheckboxGroup';
+import { Button } from '../button';
+import { Text } from '../text';
 
 export default {
   title: 'Form / Checkbox',
@@ -20,7 +23,7 @@ export const Basic = (): JSX.Element => {
     <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <div className="border-shade-light-default rounded-md border p-6 pb-8">
-          <Text variant="p" bold className="mb-1">
+          <Text as="p" weight="bold" className="mb-1">
             ラベル
           </Text>
 
@@ -46,7 +49,7 @@ export const WithNopadding = (): JSX.Element => {
     <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <div className="border-shade-light-default rounded-md border p-6 pb-8">
-          <Text variant="p" bold className="mb-1">
+          <Text as="p" weight="bold" className="mb-1">
             ラベル
           </Text>
 
@@ -63,3 +66,50 @@ export const WithNopadding = (): JSX.Element => {
     </form>
   );
 };
+
+const allPreferences = {
+  spring: '春',
+  summer: '夏',
+  autumn: '秋',
+  winter: '冬',
+} as const;
+
+const schema = yup.object({
+  preferences: yup.object(Object.fromEntries(Object.keys(allPreferences).map((k) => [k, yup.boolean()]))),
+});
+
+type FieldValue = yup.InferType<typeof schema>;
+
+export const WithCheckboxGroup = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValue>({
+    resolver: yupResolver(schema),
+  });
+  const onSubmit = useCallback((data: unknown) => {
+    console.info(data);
+  }, []);
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className={fsx(`flex flex-col gap-8`)}>
+      <CheckboxGroup
+        required
+        label="好きな季節"
+        error={!!errors['preferences']}
+        helperText={errors['preferences'] && '必須です'}
+      >
+        <Checkbox label="春" value={true} {...register('preferences.spring')} />
+        <Checkbox label="夏" value={true} {...register('preferences.summer')} />
+        <Checkbox label="秋" value={true} {...register('preferences.autumn')} />
+        <Checkbox label="冬" value={true} {...register('preferences.winter')} />
+        <Checkbox label="雨季" value={true} disabled />
+      </CheckboxGroup>
+      <Button type="submit">保存</Button>
+    </form>
+  );
+};
+
+export const CustomLabel = () => (
+  <Checkbox label={<Text size="xl" weight="bold" className="text-shade-medium-default">ラベル</Text>} />
+)
