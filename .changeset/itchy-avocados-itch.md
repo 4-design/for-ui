0cee46768e24398e78e68cd5c3a19f1f44ce5b1b
たふみ <qsf@de-liker.com>
2023-02-17 13:03:22 +0900

    fix(TextArea): 自動サイズ調整が効かないバグの修正 (#1048)
    * fix(TextArea): Fix to remove h-auto to avoid not auto-resizing
    
    * fix(TextArea): Fix to make optional unintended required properties
    
    * Add change log
    
    * Fmt
    
    * Fix to remove temporal fix since it is not caused by FUI
    
    * Update change log

---
+++ .changeset/itchy-avocados-itch.md
@@ -0,0 +1,5 @@
+---
+"@4design/for-ui": patch
+---
+
+fix(TextArea): 自動サイズ調整が効かないバグの修正
--- packages/for-ui/src/textArea/TextArea.tsx
+++ packages/for-ui/src/textArea/TextArea.tsx
@@ -103,7 +103,7 @@ export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
             minRows={rows || minRows}
             maxRows={rows || maxRows}
             className={fsx([
-              `w-full bg-shade-white-default ring-shade-medium-default ring-inset ring-1 text-r text-shade-dark-default placeholder:text-shade-light-default rounded h-auto py-2.5 px-3 font-sans font-normal placeholder:opacity-100  focus-visible:outline-none focus-visible:ring-primary-medium-active focus-visible:ring-2`,
+              `w-full bg-shade-white-default ring-shade-medium-default ring-inset ring-1 text-r text-shade-dark-default placeholder:text-shade-light-default rounded py-2.5 px-3 font-sans font-normal placeholder:opacity-100  focus-visible:outline-none focus-visible:ring-primary-medium-active focus-visible:ring-2`,
               error && `ring-negative-medium-default focus-visible:ring-negative-medium-default`,
               disabled &&
                 `bg-shade-white-disabled ring-shade-medium-disabled text-shade-dark-disabled placeholder:text-shade-light-disabled cursor-not-allowed`,
