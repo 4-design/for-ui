9bc4e4a3960d6c50d1be1eb6e707c9d675b00c9a
Qs-F <qsf@de-liker.com>
2023-02-20 17:41:12 +0900

    fix(Menu): Fix to support as props for MenuList

--- packages/for-ui/src/menu/MenuList.tsx
+++ packages/for-ui/src/menu/MenuList.tsx
@@ -1,8 +1,29 @@
-import { forwardRef } from 'react';
-import MuiMenuList, { MenuListProps } from '@mui/material/MenuList';
+import { forwardRef, ElementType, Ref } from 'react';
+import MuiMenuList, { MenuListProps as MuiMenuListProps } from '@mui/material/MenuList';
 import { fsx } from '../system/fsx';
 import { style } from './style';
 
-export const MenuList = forwardRef<HTMLUListElement, MenuListProps>(({ className, ...rest }, ref) => (
-  <MuiMenuList ref={ref} className={fsx(style, className)} {...rest} />
-));
+export type MenuListProps<P extends ElementType = 'ul'> = MuiMenuListProps<P> & {
+  /**
+   * レンダリングするコンポーネントを指定 (例: h1, p, strong)
+   * @default span
+   */
+  as?: P;
+
+  ref?: Ref<P | null>;
+};
+
+const _MenuList = <P extends ElementType = 'ul'>({
+  as,
+  className,
+  _ref,
+  ...rest
+}: MenuListProps<P> & {
+  _ref: Ref<P | null | unknown>;
+}) => <MuiMenuList component={as || 'ul'} inputRef={_ref} className={fsx(style, className)} {...rest} />;
+
+export const MenuList = forwardRef((props, ref) => <_MenuList _ref={ref} {...props} />) as unknown as <
+  P extends ElementType = 'ul'
+>(
+  props: MenuListProps<P>
+) => JSX.Element;
