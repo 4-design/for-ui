b6858f77f03529964705188801eb29d447eaa291
Qs-F <qsf@de-liker.com>
2023-02-24 16:01:43 +0900

    Add loader internal component

---
+++ packages/for-ui/src/loader/Loader.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta } from '@storybook/react/types-6-0';
+import { Loader } from './Loader';
+
+export default {
+  title: 'General / Loader',
+  component: Loader,
+} as Meta;
+
+export const Playground = {};
