dd2dbb7d954acf56c934953f8a26b27f28d05fa7
Qs-F <qsf@de-liker.com>
2023-02-24 16:28:47 +0900

    Remove loader storybook

--- packages/for-ui/src/loader/Loader.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Meta } from '@storybook/react/types-6-0';
-import { Loader } from './Loader';
-
-export default {
-  title: 'General / Loader',
-  component: Loader,
-} as Meta;
-
-export const Playground = {};
