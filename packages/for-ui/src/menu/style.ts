c64c54c897f5b6d784d326eb459518fde21c7a42
Qs-F <qsf@de-liker.com>
2023-02-28 17:47:40 +0900

    Fix to use shadow-more

--- packages/for-ui/src/menu/style.ts
+++ packages/for-ui/src/menu/style.ts
@@ -1,5 +1,5 @@
 import { fsx } from '../system/fsx';
 
 export const style = fsx(
-  `z-modal min-w-min bg-shade-white-default shadow-[rgba(0,0,0,.1)_0_8px_24px] w-full rounded px-0 py-1 divide-shade-light-default grid grid-cols-1 divide-y border border-shade-light-default`
+  `z-modal min-w-min bg-shade-white-default shadow-more w-full rounded px-0 py-1 divide-shade-light-default grid grid-cols-1 divide-y border border-shade-light-default`
 );
