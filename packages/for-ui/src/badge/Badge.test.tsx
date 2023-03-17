2315153772c7ad5a6a572a4c8c7e5832e5895fc4
Qs-F <qsf@de-liker.com>
2023-03-17 18:21:15 +0900

    Add test for badge (example)

---
+++ packages/for-ui/src/badge/Badge.test.tsx
@@ -0,0 +1,10 @@
+import { expect, describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { Badge } from './Badge';
+
+describe('Badge', () => {
+  it('renders label', () => {
+    render(<Badge label="badge" />)
+    expect(screen.getByText('badge')).toBeInTheDocument()
+  })
+})
