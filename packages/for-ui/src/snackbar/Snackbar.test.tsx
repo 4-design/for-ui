5dc1a3486af812d074109954383b57eab2b9a201
Qs-F <qsf@de-liker.com>
2023-03-23 20:34:11 +0900

    Fmt

--- packages/for-ui/src/snackbar/Snackbar.test.tsx
+++ packages/for-ui/src/snackbar/Snackbar.test.tsx
@@ -1,13 +1,13 @@
+import { config } from 'react-transition-group';
 import { describe, expect, it } from 'vitest';
 import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { config } from 'react-transition-group'
 import { Button } from '../button';
 import { Snackbar } from './Snackbar';
 
 describe('Snackbar with close button', () => {
   beforeAll(() => {
-    config.disabled = true
+    config.disabled = true;
   });
   afterAll(() => {
     config.disabled = false;
@@ -28,22 +28,24 @@ describe('Snackbar with close button', () => {
     expect(screen.getByRole('alertdialog', { description: '操作が完了しました' })).toBeInTheDocument();
   });
   it('is closed when clicking close button', async () => {
-    const user = userEvent.setup()
+    const user = userEvent.setup();
     render(<Snackbar TriggerComponent={<Button aria-haspopup="dialog">開く</Button>} message="操作が完了しました" />);
-    await user.click(screen.getByRole('button', { name: "開く" }))
-    user.click(screen.getByRole('button', { name: '閉じる' }))
-    await waitForElementToBeRemoved(screen.queryByRole("alertdialog", { description: "操作が完了しました" }), { timeout: 1000 })
+    await user.click(screen.getByRole('button', { name: '開く' }));
+    user.click(screen.getByRole('button', { name: '閉じる' }));
+    await waitForElementToBeRemoved(screen.queryByRole('alertdialog', { description: '操作が完了しました' }), {
+      timeout: 1000,
+    });
   });
 });
 
 describe('Snackbar with auto close configuration', () => {
   beforeAll(() => {
-    config.disabled = true
+    config.disabled = true;
   });
   afterAll(() => {
     config.disabled = false;
   });
-  
+
   it('appears when open props is true', async () => {
     render(<Snackbar open={true} message="操作が完了しました" autoHideDuration={100} />);
     expect(screen.getByRole('alert', { description: '操作が完了しました' })).toBeInTheDocument();
@@ -53,15 +55,23 @@ describe('Snackbar with auto close configuration', () => {
     expect(screen.queryByRole('alert', { description: '操作が完了しました' })).not.toBeInTheDocument();
   });
   it('appears when TriggerComponent is pushed', async () => {
-    const user = userEvent.setup()
-    render(<Snackbar TriggerComponent={<Button aria-haspopup="dialog">開く</Button>} message="操作が完了しました" autoHideDuration={100} />);
-    await user.click(screen.getByRole('button', { name: "開く" }))
-    expect(screen.getByRole("alert", { description: "操作が完了しました" })).toBeInTheDocument()
-  })
+    const user = userEvent.setup();
+    render(
+      <Snackbar
+        TriggerComponent={<Button aria-haspopup="dialog">開く</Button>}
+        message="操作が完了しました"
+        autoHideDuration={100}
+      />,
+    );
+    await user.click(screen.getByRole('button', { name: '開く' }));
+    expect(screen.getByRole('alert', { description: '操作が完了しました' })).toBeInTheDocument();
+  });
   it('is closed after autoHideDuration passes', async () => {
-    const user = userEvent.setup()
+    const user = userEvent.setup();
     render(<Snackbar TriggerComponent={<Button>開く</Button>} message="操作が完了しました" autoHideDuration={100} />);
-    await user.click(screen.getByRole('button', { name: "開く" }))
-    await waitForElementToBeRemoved(screen.queryByRole("alert", { description: "操作が完了しました" }), { timeout: 1000 })
+    await user.click(screen.getByRole('button', { name: '開く' }));
+    await waitForElementToBeRemoved(screen.queryByRole('alert', { description: '操作が完了しました' }), {
+      timeout: 1000,
+    });
   });
 });
