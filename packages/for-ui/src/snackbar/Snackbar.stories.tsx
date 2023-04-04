78dab1827181ea61f6fd92d3233f91f5712652cc
Qs-F <qsf@de-liker.com>
2023-04-04 16:58:49 +0900

    Add context usage

--- packages/for-ui/src/snackbar/Snackbar.stories.tsx
+++ packages/for-ui/src/snackbar/Snackbar.stories.tsx
@@ -2,7 +2,7 @@ import { Fragment, useState } from 'react';
 import { Meta } from '@storybook/react/types-6-0';
 import { Button } from '../button';
 import { Snackbar, SnackbarProps } from './Snackbar';
-import { SnackbarProvider } from './SnackbarContext';
+import { SnackbarProvider, useSnackbar } from './SnackbarContext';
 
 export default {
   title: 'Feedback / Snackbar',
@@ -13,6 +13,7 @@ export const Playground = {
   args: {
     open: true,
     message: 'テスト',
+    autoHide: true,
     autoHideDuration: null,
   },
 };
@@ -20,7 +21,7 @@ export const Playground = {
 export const Stateless = () => <Snackbar TriggerComponent={<Button>開く</Button>} message="操作が完了しました" />;
 
 export const StatelessAutoHide = () => (
-  <Snackbar TriggerComponent={<Button>開く</Button>} autoHideDuration={1000} message="操作が完了しました" />
+  <Snackbar TriggerComponent={<Button>開く</Button>} autoHide message="操作が完了しました" />
 );
 
 export const Stateful = () => {
@@ -50,15 +51,28 @@ export const StatefulAutoHide = () => {
   return (
     <Fragment>
       <Button onClick={openHandler}>開く</Button>
-      <Snackbar open={open} onClose={closeHandler} autoHideDuration={1000} message="操作が完了しました" />
+      <Snackbar open={open} onClose={closeHandler} autoHide message="操作が完了しました" />
     </Fragment>
   );
 };
 
-export const WithContext = () => (
-  <SnackbarProvider>
-    <Snackbar TriggerComponent={<Button>開く1</Button>} autoHideDuration={10} message="操作が完了しました1" />
-    <Snackbar TriggerComponent={<Button>開く2</Button>} message="操作が完了しました2" />
-    <Snackbar TriggerComponent={<Button>開く3</Button>} message="操作が完了しました3" />
-  </SnackbarProvider>
-);
+export const WithContext = () => {
+  const App = () => {
+    const { openSnackbar } = useSnackbar();
+    return (
+      <div className="flex gap-2">
+        <Button onClick={() => openSnackbar({ autoHide: true, message: '5秒後に閉じるSnackbar' })}>
+          自動的に閉じるSnackbarを開く
+        </Button>
+        <Button onClick={() => openSnackbar({ message: '閉じるボタンを押さないと消えないSnackbar' })}>
+          自動的に閉じないSnackbarを開く
+        </Button>
+      </div>
+    );
+  };
+  return (
+    <SnackbarProvider>
+      <App />
+    </SnackbarProvider>
+  );
+};
