a0130f0f05310b4988dc1fbcbf80e27510eab8fb
Qs-F <qsf@de-liker.com>
2023-04-18 01:15:34 +0800

    Fix Drawer component design

--- packages/for-ui/src/drawer/Drawer.tsx
+++ packages/for-ui/src/drawer/Drawer.tsx
@@ -1,127 +1,178 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useCallback, useRef, useState } from 'react';
+import { ButtonHTMLAttributes, CSSProperties, FC, ReactNode, useCallback, useState } from 'react';
 import { MdClose, MdMoreVert } from 'react-icons/md';
 import MuiDrawer, { DrawerProps as MuiDrawerProps } from '@mui/material/Drawer';
 import { Button } from '../button';
 import { fsx } from '../system/fsx';
 
-type Props = MuiDrawerProps & {
-  headerChildren?: React.ReactNode;
-  defaultWidth?: number;
-  defaultHeight?: number;
-  minWidth?: number;
-  minHeight?: number;
-  onClose?: () => void;
-  className?: string;
-};
-
-const _minWidth = 240;
-
-export const DrawerAnchor = {
+export const drawerAnchorPositions = {
   left: 'left',
   right: 'right',
 } as const;
-export type DrawerAnchor = (typeof DrawerAnchor)[keyof typeof DrawerAnchor];
 
-export const Drawer: React.FC<Props> = ({
-  open,
-  anchor = DrawerAnchor.right,
+export type DrawerAnchorPosition = (typeof drawerAnchorPositions)[keyof typeof drawerAnchorPositions];
+
+type Props = MuiDrawerProps & {
+  /**
+   * Drawerを開く方向を指定
+   *
+   * @default 'right'
+   */
+  anchor?: DrawerAnchorPosition;
+
+  /**
+   * Drawerをリサイズ不可能にする場合に指定
+   *
+   * @default false
+   */
+  disableResizing?: boolean;
+
+  /**
+   * ヘッダー部分に置くコンテンツを指定
+   */
+  headerChildren?: ReactNode;
+
+  /**
+   * Drawerの固定の横幅を指定
+   *
+   * 指定するとdefaultWidthやminWidthは無視されリサイズできなくなります。
+   *
+   * @default 320
+   */
+  width?: CSSProperties['width'];
+
+  /**
+   * Drawerのデフォルトの横幅を指定
+   *
+   * @default 320
+   */
+  defaultWidth?: number;
+
+  /**
+   * Drawerの最小の横幅を指定
+   *
+   * @default "fit-content"
+   */
+  minWidth?: CSSProperties['minWidth'];
+
+  /**
+   * Drawerを閉じたときに実行する関数を指定
+   */
+  onClose?: () => void;
+
+  className?: string;
+};
+
+const defaultMinWidth = 320;
+
+const DragHandle: FC<{ dragging: boolean } & ButtonHTMLAttributes<HTMLButtonElement>> = ({
+  dragging,
+  ...rest
+}) => (
+  <button
+    {...rest}
+    aria-label="左右にドラッグしてドロワーのサイズを調整"
+    title="左右にドラッグしてドロワーのサイズを調整"
+    tabIndex={-1}
+    className={fsx([
+      `user-select-[none] bg-shade-white-default hover:bg-shade-white-hover [&_svg]:fill-shade-medium-default focus-visible:bg-shade-white-active border-shade-light-default sticky top-0 flex h-full w-4 cursor-[ew-resize] items-center justify-center border-x focus-visible:outline-none`,
+      dragging && `bg-shade-white-active [&_svg]:fill-shade-medium-active`,
+    ])}
+  >
+    <MdMoreVert size={24} className={fsx(`shrink-0`)} />
+  </button>
+);
+
+export const Drawer: FC<Props> = ({
+  anchor = drawerAnchorPositions.right,
   headerChildren,
-  defaultWidth = _minWidth,
-  minWidth = _minWidth,
+  defaultWidth = defaultMinWidth,
+  minWidth = `fit-content`,
+  width: fixedWidth,
   children,
   onClose,
   className,
   ...rest
 }) => {
-  const ref = useRef(null);
+  const [dragging, setDragging] = useState(false);
   const [width, setWidth] = useState(defaultWidth);
 
-  const handleClose = useCallback(() => {
-    if (onClose) onClose();
-  }, [onClose]);
-
-  const handleMouseMove = useCallback(
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (e: any) => {
-      switch (anchor) {
-        case DrawerAnchor.left: {
-          const newWidth = e.pageX;
-          if (newWidth > minWidth && newWidth < window.innerWidth) {
-            setWidth(newWidth);
-          }
-
-          break;
-        }
-        case DrawerAnchor.right: {
-          const newWidth = window.innerWidth - e.pageX;
-          if (newWidth > minWidth && newWidth < window.innerWidth) {
-            setWidth(newWidth);
-          }
-
-          break;
-        }
-      }
+  const handleDrag = useCallback(
+    (e: MouseEvent) => {
+      setWidth(
+        {
+          left: e.pageX + 8,
+          right: window.innerWidth - e.pageX + 8,
+        }[anchor],
+      );
     },
     [anchor],
   );
 
-  const handleMouseUp = useCallback(() => {
-    document.removeEventListener('mouseup', handleMouseUp, true);
-    document.removeEventListener('mousemove', handleMouseMove, true);
+  const onDragEnd = useCallback(() => {
+    document.removeEventListener('pointerup', onDragEnd, true);
+    document.removeEventListener('pointermove', handleDrag, true);
+    setDragging(false);
   }, []);
 
-  const handleMouseDown = useCallback(() => {
-    document.addEventListener('mouseup', handleMouseUp, true);
-    document.addEventListener('mousemove', handleMouseMove, true);
+  const onDragStart = useCallback(() => {
+    document.addEventListener('pointerup', onDragEnd, true);
+    document.addEventListener('pointermove', handleDrag, true);
+    setDragging(true);
   }, []);
 
   return (
     <MuiDrawer
-      open={open}
       anchor={anchor}
+      onClose={onClose}
+      components={{
+        Root: 'aside',
+      }}
       classes={{
-        root: fsx('shadow-drawer z-modal', className),
-        paper: fsx('p-6 [&.MuiBackdrop-root]:bg-transparent'),
+        root: fsx([`z-modal [&_.MuiBackdrop-root]:bg-[#0000004D]`, dragging && `cursor-[ew-resize]`, className]), 
+        paper: fsx([
+          `shadow-more border-shade-light-default flex h-full max-w-[calc(100%_-_16px)] flex-row`,
+          {
+            left: fixedWidth && `border-r`,
+            right: fixedWidth && `border-l`,
+          }[anchor],
+        ]),
       }}
       PaperProps={{
         sx: {
-          width,
-          height: '100%',
+          minWidth,
+          width: fixedWidth || width,
         },
       }}
-      onClose={handleClose}
+      SlideProps={{
+        easing: {
+          enter: 'cubic-bezier(.05, .95, .05, .95)',
+          exit: 'cubic-bezier(.95, .05, .05, .95)',
+        },
+      }}
+      transitionDuration={200}
       {...rest}
     >
-      <div className="flex items-center">
-        <Button variant="text" size="medium" startIcon={<MdClose size={16} />} onClick={handleClose}>
-          閉じる
-        </Button>
-
-        {headerChildren && <div className="ml-auto">{headerChildren}</div>}
-      </div>
-
-      <div
-        onMouseDown={handleMouseDown}
-        ref={ref}
-        className={fsx([
-          'user-select[none] absolute top-0 h-full w-9 cursor-[ew-resize] px-3',
-          anchor === 'left' ? '-right-3' : '-left-3',
-        ])}
-      >
-        <span
-          className={fsx([
-            'border-shade-light-default hover:bg-shade-white-hover block h-full w-full',
-            anchor === DrawerAnchor.left ? 'border-r' : 'border-l',
-          ])}
+      {!fixedWidth && anchor === 'right' && (
+        <DragHandle onPointerDown={onDragStart} dragging={dragging} />
+      )}
+      <div className={fsx(`flex w-full flex-col`)}>
+        <nav
+          className={fsx(
+            `border-shade-light-default bg-shade-white-default sticky top-0 flex items-center justify-between gap-2 border-b p-2`,
+          )}
         >
-          <MdMoreVert
-            size="24"
-            className={fsx(['relative h-full', anchor === 'left' ? 'right-[6px]' : 'left-[-6px]'])}
-          />
-        </span>
+          <Button variant="text" size="medium" intention="subtle" onClick={onClose}>
+            <MdClose />
+            閉じる
+          </Button>
+          {headerChildren && <div className={fsx(`flex items-center gap-2`)}>{headerChildren}</div>}
+        </nav>
+        <article className={fsx(`flex flex-col gap-1 p-4`)}>{children}</article>
       </div>
-      <div className="overflow-wrap[break-word] mt-3">{children}</div>
+      {!fixedWidth && anchor === 'left' && (
+        <DragHandle onPointerDown={onDragStart} dragging={dragging} />
+      )}
     </MuiDrawer>
   );
 };
