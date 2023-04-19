942a528db474e9bd75fdc2d26b327c9241b8ff25
たふみ <qsf@de-liker.com>
2023-04-19 16:21:28 +0900

    fix(Badge): レイアウト修正 (#1120)
    * Fix Badge to avoid unintended layout modification used in flexbox
    
    * Add change log

--- packages/for-ui/src/badge/TextBadge.tsx
+++ packages/for-ui/src/badge/TextBadge.tsx
@@ -6,7 +6,7 @@ import { BadgeProps } from './Badge';
 export const TextBadge: FC<Omit<BadgeProps, 'variant'>> = ({ intention = 'subtle', icon, label, className }) => (
   <span
     className={fsx([
-      `flex w-fit flex-row items-center justify-center gap-1 rounded px-2`,
+      `flex h-fit w-fit flex-row items-center justify-center gap-1 rounded px-2`,
       {
         subtle: `bg-shade-light-default text-shade-dark-default`,
         shade: `bg-shade-white-default text-shade-dark-default`,
