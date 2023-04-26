92ca0827a4b02e0d0145bd6747b9d7e5ddc10740
Qs-F <qsf@de-liker.com>
2023-04-26 16:48:23 +0900

    fix(example/vite-app): Fix vite app

--- example/vite-app/src/main.tsx
+++ example/vite-app/src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
-import './styles/index.css';
+import './styles/global.css';
+import './styles/forty.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
