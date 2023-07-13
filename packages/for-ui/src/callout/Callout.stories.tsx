0f2f4d9596d9d85e2061d59748c72aa046afda86
Qs-F <qsf@de-liker.com>
2023-07-13 10:51:04 +0900

    Add Default story

--- packages/for-ui/src/callout/Callout.stories.tsx
+++ packages/for-ui/src/callout/Callout.stories.tsx
@@ -1,5 +1,6 @@
 import { MdErrorOutline } from 'react-icons/md';
 import { Meta } from '@storybook/react/types-6-0';
+import { Text } from '../text';
 import { Callout } from './Callout';
 
 const sampleIcons = {
@@ -25,3 +26,34 @@ export const Playground = {
     icon: 'MdErrorOutline',
   },
 };
+
+export const Default = () => (
+  <div className="flex flex-col gap-4">
+    <Text as="h3" size="l">
+      アイコンなし
+    </Text>
+    <Callout intention="subtle">自動診断がオフになっています。</Callout>
+    <Callout intention="positive">自動診断がオフになっています。</Callout>
+    <Callout intention="negative">自動診断がオフになっています。</Callout>
+    <Callout intention="notice">自動診断がオフになっています。</Callout>
+    <Callout intention="informative">自動診断がオフになっています。</Callout>
+    <Text as="h3" size="l">
+      アイコン付き
+    </Text>
+    <Callout icon={<MdErrorOutline />} intention="subtle">
+      自動診断がオフになっています。
+    </Callout>
+    <Callout icon={<MdErrorOutline />} intention="positive">
+      自動診断がオフになっています。
+    </Callout>
+    <Callout icon={<MdErrorOutline />} intention="negative">
+      自動診断がオフになっています。
+    </Callout>
+    <Callout icon={<MdErrorOutline />} intention="notice">
+      自動診断がオフになっています。
+    </Callout>
+    <Callout icon={<MdErrorOutline />} intention="informative">
+      自動診断がオフになっています。
+    </Callout>
+  </div>
+);
