8681d6325ead80f2751f990315bbbbb687b959c5
Qs-F <qsf@de-liker.com>
2023-07-24 09:33:53 +0900

    Fmt

--- packages/for-ui/src/table/TablePagination.test.tsx
+++ packages/for-ui/src/table/TablePagination.test.tsx
@@ -18,40 +18,68 @@ describe('Pagination', () => {
     const user = userEvent.setup();
     const onChangePage = vi.fn((page: number) => page);
     const onClickFirstPageButton = vi.fn();
-    render(<Pagination total={100} defaultPage={50} onChangePage={onChangePage} onClickFirstPageButton={onClickFirstPageButton} />);
-    await user.click(screen.getByRole('button', { name: '最初のページへ移動' }))
+    render(
+      <Pagination
+        total={100}
+        defaultPage={50}
+        onChangePage={onChangePage}
+        onClickFirstPageButton={onClickFirstPageButton}
+      />,
+    );
+    await user.click(screen.getByRole('button', { name: '最初のページへ移動' }));
     expect(onChangePage).toHaveBeenCalledOnce();
     expect(onChangePage).toHaveLastReturnedWith(1);
     expect(onClickFirstPageButton).toHaveBeenCalledOnce();
-  })
+  });
   it('calls event handlers when last page button is clicked', async () => {
     const user = userEvent.setup();
     const onChangePage = vi.fn((page: number) => page);
     const onClickLastPageButton = vi.fn();
-    render(<Pagination total={100} defaultPage={50} onChangePage={onChangePage} onClickLastPageButton={onClickLastPageButton} />);
-    await user.click(screen.getByRole('button', { name: '最後のページへ移動' }))
+    render(
+      <Pagination
+        total={100}
+        defaultPage={50}
+        onChangePage={onChangePage}
+        onClickLastPageButton={onClickLastPageButton}
+      />,
+    );
+    await user.click(screen.getByRole('button', { name: '最後のページへ移動' }));
     expect(onChangePage).toHaveBeenCalledOnce();
     expect(onChangePage).toHaveLastReturnedWith(100);
     expect(onClickLastPageButton).toHaveBeenCalledOnce();
-  })
+  });
   it('calls event handlers when previous page button is clicked', async () => {
     const user = userEvent.setup();
     const onChangePage = vi.fn((page: number) => page);
     const onClickPreviousPageButton = vi.fn();
-    render(<Pagination total={100} defaultPage={50} onChangePage={onChangePage} onClickPreviousPageButton={onClickPreviousPageButton} />);
-    await user.click(screen.getByRole('button', { name: '前のページへ移動' }))
+    render(
+      <Pagination
+        total={100}
+        defaultPage={50}
+        onChangePage={onChangePage}
+        onClickPreviousPageButton={onClickPreviousPageButton}
+      />,
+    );
+    await user.click(screen.getByRole('button', { name: '前のページへ移動' }));
     expect(onChangePage).toHaveBeenCalledOnce();
     expect(onChangePage).toHaveLastReturnedWith(49);
     expect(onClickPreviousPageButton).toHaveBeenCalledOnce();
-  })
+  });
   it('calls event handlers when next page button is clicked', async () => {
     const user = userEvent.setup();
     const onChangePage = vi.fn((page: number) => page);
     const onClickNextPageButton = vi.fn();
-    render(<Pagination total={100} defaultPage={50} onChangePage={onChangePage} onClickNextPageButton={onClickNextPageButton} />);
-    await user.click(screen.getByRole('button', { name: '次のページへ移動' }))
+    render(
+      <Pagination
+        total={100}
+        defaultPage={50}
+        onChangePage={onChangePage}
+        onClickNextPageButton={onClickNextPageButton}
+      />,
+    );
+    await user.click(screen.getByRole('button', { name: '次のページへ移動' }));
     expect(onChangePage).toHaveBeenCalledOnce();
     expect(onChangePage).toHaveLastReturnedWith(51);
     expect(onClickNextPageButton).toHaveBeenCalledOnce();
-  })
-})
+  });
+});
