f7e68cc94672df5e10dd51d8d5da0a52a3348a42
Qs-F <qsf@de-liker.com>
2023-08-04 19:47:42 +0900

    Update to storybook v7

--- packages/for-ui/tailwind.config.js
+++ packages/for-ui/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  important: ':is(#root, #docs-root)',
+  important: ':is(#root, #storybook-root, #storybook-docs)',
   presets: [require('./tailwind.config.base.js')],
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
 };
