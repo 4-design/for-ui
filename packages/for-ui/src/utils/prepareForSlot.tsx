dd76f90a40b3488eaa5fad07f3174ebe2274bb5e
Qs-F <qsf@de-liker.com>
2023-12-19 18:36:12 +0900

    Add prepareForSlot util from @mui/base

---
+++ packages/for-ui/src/utils/prepareForSlot.tsx
@@ -0,0 +1,15 @@
+// Retrieved from https://github.com/mui/material-ui/blob/master/packages/mui-base/src/utils/prepareForSlot.tsx
+
+import * as React from 'react';
+
+export function prepareForSlot<ComponentType extends React.ElementType>(Component: ComponentType) {
+  type Props = React.ComponentProps<ComponentType>;
+
+  return React.forwardRef<HTMLElement, Props>(function Slot(props, ref) {
+    const { ownerState: _, ...other } = props;
+    return React.createElement<Props>(Component, {
+      ...(other as Props),
+      ref,
+    });
+  });
+}
